a13bda96a92a6e34fa71ddc638711d1942afa3f5
GMartinez0210 <genaro010204@gmail.com>
2022-05-11 09:27:22 -0500

    Cloname p :v

---
+++ PROJECT/src/util/MySQLConexion8.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConexion8 {
+	public static Connection getConexion() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			String url = "jdbc:mysql://localhost:3306/Seguridad_Informatica?useSSL=false&useTimezone=true&serverTimezone=UTC";
+			String usr = /*your user*/;
+			String psw = /*your password*/;
+			con = DriverManager.getConnection(url, usr, psw);
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error >> Driver no Instalado!!" + e.getMessage());
+		} catch (SQLException e) {
+			System.out.println("Error >> de conexión con la BD" + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error >> general : " + e.getMessage());
+		} 
+		return con;
+	}
+
+}
